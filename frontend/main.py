4b37a5fc6335d8482a40ee290216446c6a581fb2
Precious Ojogu <nkangprecious26@gmail.com>
2024-03-26 12:09:59 +0100

    working on the frontend using the flet framework

---
+++ frontend/main.py
@@ -0,0 +1,20 @@
+import flet as ft
+
+def main(page):
+    header = ft.Text()
+    header.value = "NOTEBOOK"
+    header.color = "red"
+    page.add(header)
+    
+    def add_clicked(e):
+        page.add(ft.Checkbox(label=new_task.value))
+        new_task.value = ""
+        new_task.focus()
+        new_task.update()
+
+    new_task = ft.TextField(hint_text="Whats needs to be done?", width=300)
+    page.add(ft.Row([new_task, ft.ElevatedButton("Add", on_click=add_clicked)]))
+
+ft.app(target=main)
+    
+# flet run main.py -d
